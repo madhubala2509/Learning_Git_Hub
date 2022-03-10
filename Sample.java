14b5d0ac934409f8710a672f0a3ea84f87a0fede
madhubala2509 <smadhubala79@gmail.com>
2022-03-10 11:07:59 +0530

    Added a new branch from local machine with methods add,sub

--- Sample.java
+++ Sample.java
@@ -1,6 +1,14 @@
 
 public class Sample {
+    //adding few lines in new branch
+    static int add(int a,int b){
+        return a+b;
+    }
+    static int suv(int a,int b){
+        return a-b;
+    }
     public static void main(String args[]){
         System.out.println("Hello World");
     }
+
 }
