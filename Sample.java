e33d2e7b0546a5f164929a4474c282ce58b7e6f4
madhubala2509 <smadhubala79@gmail.com>
2022-03-10 11:30:40 +0530

    After merging

--- Sample.java
+++ Sample.java
@@ -1,12 +1,12 @@
-
+#Resolved conflict and merged branch
 public class Sample {
     //adding few lines in new branch
     static int add(int a,int b){
         return a+b;
     }
-    static int sub(int a,int b){
-        return a-b;
-
+    static int sub(int a,int b) {
+        return a - b;
+    }
     //adding few lines in main branch in git
     static int mul(int a,int b){
         return a*b;
