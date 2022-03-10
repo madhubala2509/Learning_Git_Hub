54d07eaef81494a7a31b5d21ba8140291660ecb4
madhubala2509 <100773688+madhubala2509@users.noreply.github.com>
2022-03-10 11:13:34 +0530

    Update Sample.java
    Added mul,div method in main branch using git

--- Sample.java
+++ Sample.java
@@ -1,5 +1,12 @@
 
 public class Sample {
+    //adding few lines in main branch in git
+    static int mul(int a,int b){
+        return a*b;
+    }
+    static int div(int a,int b){
+        return a/b;
+    }
     public static void main(String args[]){
         System.out.println("Hello World");
     }
